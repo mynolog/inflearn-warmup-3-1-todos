576c82015a92c8473ac19c6ff14901bfc92d4eb3
mynolog <minholee.log@gmail.com>
2025-03-05 22:40:20 +0900

    chore: 프로젝트 기본 환경 설정 구축 및 의존성 설치

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,16 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
+import withMT from '@material-tailwind/react/utils/withMT'
+import typography from '@tailwindcss/typography'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './utils/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+  theme: {},
+  plugins: [typography],
+}
+
+export default withMT(config)
