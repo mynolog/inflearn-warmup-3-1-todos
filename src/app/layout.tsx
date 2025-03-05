e98fb9f97e828ecc9abdbb52cd68538b2b28f525
mynolog <minholee.log@gmail.com>
2025-03-05 23:28:46 +0900

    refactor: Metarial Tailwind 프로바이더 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { ThemeProvider } from '@/app/config/theme-provider'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,19 +15,17 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <ThemeProvider>
-      <html lang="en">
-        <head>
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-            integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
-            crossOrigin="anonymous"
-            referrerPolicy="no-referrer"
-          />
-        </head>
-        <body className={`${inter.className} antialiased`}>{children}</body>
-      </html>
-    </ThemeProvider>
+    <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+          integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
   )
 }
--- src/app/ui.tsx
+++ src/app/ui.tsx
@@ -3,7 +3,7 @@
 export default function UI() {
   return (
     <div className="w-2/3 mx-auto flex flex-col items-center gap-3 py-10">
-      <h2>Next Todos</h2>
+      <h2 className="text-xl font-bold">Next Todos</h2>
     </div>
   )
 }
